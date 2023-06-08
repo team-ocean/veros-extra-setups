d24a11086158fd105cc2e7a93b7e294d57eb1bf7
Dion Häfner <dion.haefner@simulation.science>
2023-06-08 13:16:12 +0200

    [docs] pivot to furo theme

--- doc/conf.py
+++ doc/conf.py
@@ -96,17 +96,16 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-import sphinx_rtd_theme
-
-html_theme = "sphinx_rtd_theme"
-
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_theme = "furo"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+    "light_logo": "veros-logo-400px.png",
+    "dark_logo": "veros-logo-bright-400px.png",
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
