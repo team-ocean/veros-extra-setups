a21df10c767f347568d12bf7acdd95de7d9e8e0b
Dion Häfner <dion.haefner@simulation.science>
2023-10-12 16:19:56 +0200

    require python 3.8

--- setup.py
+++ setup.py
@@ -16,10 +16,11 @@ Development Status :: 3 - Alpha
 Intended Audience :: Science/Research
 License :: OSI Approved :: MIT License
 Programming Language :: Python :: 3
-Programming Language :: Python :: 3.6
-Programming Language :: Python :: 3.7
 Programming Language :: Python :: 3.8
 Programming Language :: Python :: 3.9
+Programming Language :: Python :: 3.10
+Programming Language :: Python :: 3.11
+Programming Language :: Python :: 3.12
 Programming Language :: Python :: Implementation :: CPython
 Topic :: Scientific/Engineering
 Operating System :: Microsoft :: Windows
@@ -46,7 +47,7 @@ setup(
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://veros-extra-setups.readthedocs.io',
-    python_requires='>=3.6',
+    python_requires='>=3.8',
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
     packages=find_packages(),
